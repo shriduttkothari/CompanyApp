d7010a10abb4ed45d9b937c65761a0b22ac45d40
Shridutt Kothari <shriduttkothari@gmail.com>
2019-12-21 00:17:36 +0530

    Created rest client to use retrofit for making REST api invocations

---
+++ CompanyDemoApp/app/src/main/java/com/viseo/companyapp/com/viseo/companyapp/rest/RestClient.kt
@@ -0,0 +1,26 @@
+package com.viseo.companyapp.com.viseo.companyapp.rest
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Singleton Kotlin Class for Rest operation from the app
+ *
+ * @author Shridutt.Kothari
+ */
+object RestClient {
+
+    private val BASE_URL = "https://next.json-generator.com/api/json/get/Vk-LhK44U"
+    private var mRetrofit: Retrofit? = null
+
+    val client: Retrofit
+        get() {
+            if (mRetrofit == null) {
+                mRetrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return this.mRetrofit!!
+        }
+}
