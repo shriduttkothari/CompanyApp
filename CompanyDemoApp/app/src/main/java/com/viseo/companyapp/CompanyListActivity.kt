4adb01fb5e8610d96e8d7a7b635893294012d975
Shridutt Kothari <shriduttkothari@gmail.com>
2019-12-21 00:05:22 +0530

    Refactored MainActivity to CompanyListActivity

--- CompanyDemoApp/app/src/main/java/com/viseo/companyapp/CompanyListActivity.kt
+++ CompanyDemoApp/app/src/main/java/com/viseo/companyapp/CompanyListActivity.kt
@@ -3,10 +3,10 @@ package com.viseo.companyapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CompanyListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_company_list)
     }
 }
